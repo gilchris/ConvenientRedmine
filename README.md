534b281c6f7ecfaa199c7e06ab87ca3e81911a1b
Changgun Lee <lazysense@gmail.com>
2018-09-26 03:47:35 +0900

    Add option window

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ chrome extension for adding advantages of redmine
 ## Features
 
 ### Issue list coloring
+![Issue coloring](docs/issue_coloring.jpeg)
 
 ### Display detail time on detail issue view
 
--- src/option.js
+++ src/option.js
@@ -1,24 +1,93 @@
-(function () {
-    var $ = document.getElementById,
-        msg = chrome.i18n.getMessage,
+$(function () {
+    var msg = chrome.i18n.getMessage,
         initOptions = {},
-        elWorkingDomain = $("optWorkingDomain");
+        elWorkingDomain = $("#optWorkingDomain"),
+        elSaveButton = $("#btnSave"),
+        elListColor1 = $("#listColor1"),
+        elListColor2 = $("#listColor2"),
+        elListColor3 = $("#listColor3"),
+        elListColor4 = $("#listColor4"),
+        elListColor5 = $("#listColor5"),
+        elListColor6 = $("#listColor6"),
+        elListColor7 = $("#listColor7"),
+        elListColor8 = $("#listColor8"),
+        elListColor9 = $("#listColor9"),
+        elListColor10 = $("#listColor10");
 
-    $("lblWorkingDomain").innerHTML = msg("optionWorkingDomain");
-    elWorkingDomain.setAttribute("placeholder", msg("optionWorkingDomain_placeholder"));
-    $("btnSave").innerHTML = msg("buttonSave");
+    $("h1").text(msg("extOptionTitle"));
+    $("#lblWorkingDomain").text(msg("optionWorkingDomain"));
+    elWorkingDomain.attr("placeholder", msg("optionWorkingDomain_placeholder"));
+    elSaveButton.text(msg("buttonSave"));
+
+    $("#lblStateColorMaybeDescription").text(msg("stateColorMaybeDescription"));
+    $("#lblListColor1").text(msg("stateColor1"));
+    $("#lblListColor2").text(msg("stateColor2"));
+    $("#lblListColor3").text(msg("stateColor3"));
+    $("#lblListColor4").text(msg("stateColor4"));
+    $("#lblListColor5").text(msg("stateColor5"));
+    $("#lblListColor6").text(msg("stateColor6"));
+    $("#lblListColor7").text(msg("stateColor7"));
+    $("#lblListColor8").text(msg("stateColor8"));
+    $("#lblListColor9").text(msg("stateColor9"));
+    $("#lblListColor10").text(msg("stateColor10"));
+    $("#descriptionListColor1").text(msg("stateColorMaybe1"));
+    $("#descriptionListColor2").text(msg("stateColorMaybe2"));
+    $("#descriptionListColor3").text(msg("stateColorMaybe3"));
+    $("#descriptionListColor4").text(msg("stateColorMaybe4"));
+    $("#descriptionListColor5").text(msg("stateColorMaybe5"));
+    $("#descriptionListColor6").text(msg("stateColorMaybe6"));
+    $("#descriptionListColor7").text(msg("stateColorMaybe7"));
+    $("#descriptionListColor8").text(msg("stateColorMaybe8"));
+    $("#descriptionListColor9").text(msg("stateColorMaybe9"));
+    $("#descriptionListColor10").text(msg("stateColorMaybe10"));
 
     chrome.storage.sync.get({
-        "workingDomain": ".*redmine.*"
+        "workingDomain": ".*redmine.*",
+        "listColors": {
+            "1": "#aaeeaa", // 신규
+            "2": "#bbffbb", // 진행
+            "3": "#cccccc", // 해결
+            "4": "#ffff99", // 의견
+            "5": "#aaaaaa", // 완료
+            "6": "#bbbbbb", // 거절 (Reject)
+            "7": "#ffffff", // ??
+            "8": "#bbffbb", // 다시 열림 (Reopened)
+            "9": "#eeffaa", // 보류
+            "10": "#ffff99" // 의견 (Need feedback)
+        }
     }, function (options) {
         initOptions = options;
 
-        elWorkingDomain.setAttribute("value", initOptions["workingDomain"]);
+        elWorkingDomain.attr("value", initOptions["workingDomain"]);
+
+        elListColor1.val(initOptions["listColors"]["1"]);
+        elListColor2.val(initOptions["listColors"]["2"]);
+        elListColor3.val(initOptions["listColors"]["3"]);
+        elListColor4.val(initOptions["listColors"]["4"]);
+        elListColor5.val(initOptions["listColors"]["5"]);
+        elListColor6.val(initOptions["listColors"]["6"]);
+        elListColor7.val(initOptions["listColors"]["7"]);
+        elListColor8.val(initOptions["listColors"]["8"]);
+        elListColor9.val(initOptions["listColors"]["9"]);
+        elListColor10.val(initOptions["listColors"]["10"]);
     });
 
-    $("btnSave").onclick = function () {
+    elSaveButton.click(function () {
         chrome.storage.sync.set({
-            "workingDomain": elWorkingDomain.getAttribute("value")
-        })
-    };
-})();
+            "workingDomain": elWorkingDomain.val(),
+            "listColors": {
+                "1": elListColor1.val(),
+                "2": elListColor2.val(),
+                "3": elListColor3.val(),
+                "4": elListColor4.val(),
+                "5": elListColor5.val(),
+                "6": elListColor6.val(),
+                "7": elListColor7.val(),
+                "8": elListColor8.val(),
+                "9": elListColor9.val(),
+                "10": elListColor10.val()
+            }
+        });
+    });
+
+});
