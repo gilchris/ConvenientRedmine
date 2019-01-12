2a35ed9a5c58765186042d021bc0141d8643a06b
Changgun Lee <lazysense@gmail.com>
2019-01-13 05:51:00 +0900

    Fix when first time after installed, not loading options on option page

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-var options = {
-    "workingDomain": ".*redmine.*",
-    "listColors": {
-        "1": "#aaeeaa", // 신규
-        "2": "#bbffbb", // 진행
-        "3": "#cccccc", // 해결
-        "4": "#ffff99", // 의견
-        "5": "#aaaaaa", // 완료
-        "6": "#bbbbbb", // 거절 (Reject)
-        "7": "#ffffff", // ??
-        "8": "#bbffbb", // 다시 열림 (Reopened)
-        "9": "#eeffaa", // 보류
-        "10": "#ffff99" // 의견 (Need feedback)
-    }
-};
+var optionKeys = ["workingDomain", "listColors"],
+    defaultOptions = {
+        "workingDomain": ".*redmine.*",
+        "listColors": {
+            "1": "#aaeeaa", // 신규
+            "2": "#bbffbb", // 진행
+            "3": "#cccccc", // 해결
+            "4": "#ffff99", // 의견
+            "5": "#aaaaaa", // 완료
+            "6": "#bbbbbb", // 거절 (Reject)
+            "7": "#ffffff", // ??
+            "8": "#bbffbb", // 다시 열림 (Reopened)
+            "9": "#eeffaa", // 보류
+            "10": "#ffff99" // 의견 (Need feedback)
+        }
+    },
+    options = {};
 function loadOptions(callback) {
-    chrome.storage.sync.get(options, function (_options) {
-        options = _options;
+    chrome.storage.sync.get(optionKeys, function (_options) {
+        options = Object.assign(_options, defaultOptions);
         if (typeof callback === 'function') {
-            callback(options);
+            callback(_options);
         }
     });
 }
@@ -25,11 +27,12 @@ function saveOptions(_options) {
     chrome.storage.sync.set(_options);
     options = _options;
 }
-loadOptions();
 
 chrome.runtime.onInstalled.addListener(function () {
-    saveOptions(options);
+    saveOptions(defaultOptions);
 });
+//chrome.storage.sync.remove(optionKeys);
+loadOptions();
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
     if (details.url.match(new RegExp(options.workingDomain, "i"))) {
